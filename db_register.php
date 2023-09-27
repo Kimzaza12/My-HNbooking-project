f14fe358af5a427e2b3f4d4a03e3ae728eb3cd95
Kimzaza12 <gamekak12@gmail.com>
2023-09-28 02:46:15 +0700

    add register fix

--- db_register.php
+++ db_register.php
@@ -21,16 +21,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $P_number = $_POST['P_number'];
     $Email = $_POST['Email'];
 
+
+    $sql = "SELECT * FROM book WHERE HN = '$hn'";
+
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // ถ้ามีการจองห้องพักอยู่แล้ว
+        $messagehn = "HN $hn เคยลงทะเบียนไว้อยู่แล้ว";
+        echo "<script>alert('".$messagehn."')</script>";
+        echo "<script>window.location.href='summit_1.php'</script>";
+    } else
+
     // เพิ่มข้อมูลลงในตาราง "register"
     $sql = "INSERT INTO register (HN, ID_number, S_name, Department,P_number, Email) VALUES ('$HN', '$ID_number', '$S_name', '$Department','$P_number', '$Email')";
+    $result = $conn->query($sql);
 
     if ($conn->query($sql) === TRUE) {
+        $messagetrue = "ลงทะเบียนสำเร็จ";
+        echo "<script>alert('".$messagetrue."')</script>";
         header("Location: main.php"); // เปลี่ยนทิศทางไปยัง confirm.php
         exit(); 
-    } else {
-        echo "เกิดข้อผิดพลาดในการบันทึกข้อมูล: " . $conn->error;
     }
 }
+
 
 // ปิดการเชื่อมต่อกับฐานข้อมูล
 $conn->close();
